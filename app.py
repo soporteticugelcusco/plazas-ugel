3f443da0d1c7cc4b0b593ea2d9406996bc3ac5f1
soporteticugelcusco <soporteticugelcusco@gmail.com>
2026-02-18 11:28:57 -0500

    v2180226
    Changed background color for 'ADJUDICADA' and 'DISPONIBLE' states.

--- app.py
+++ app.py
@@ -32,10 +32,10 @@ def estilo_fila(row):
     
     if valor == 'ADJUDICADA':
         # Rojo claro para toda la fila
-        return ['background-color: #FFFF99; color: #000000'] * len(row)
+        return ['background-color: #FF0000; color: #000000'] * len(row)
     elif valor == 'DISPONIBLE':
         # Verde claro para toda la fila
-        return ['background-color: #6CD900; color: #000000'] * len(row)
+        return ['background-color: ; color: #000000'] * len(row)
     return [''] * len(row)
 
 # 3. CABECERA
