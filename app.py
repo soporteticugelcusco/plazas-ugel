ab6668d3ccabe40f789d09cd9604d8a6db19c20b
soporteticugelcusco <soporteticugelcusco@gmail.com>
2026-02-19 10:19:40 -0500

    Increase cache data TTL from 3 to 5 secondsactualizacion 5

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # 2. FUNCIONES
-@st.cache_data(ttl=3)
+@st.cache_data(ttl=5)
 def cargar_datos(url):
     csv_url = url.replace('/edit?usp=sharing', '/export?format=csv')
     return pd.read_csv(csv_url)
